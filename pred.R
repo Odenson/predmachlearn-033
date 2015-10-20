9754fc605ce177c0f1c1f196d7511cf49fc0e31e
Frank Jung <frankhjung@linux.com>
2015-10-20 17:40:58 +1100

    moved validation code here

--- pred.R
+++ pred.R
@@ -1,5 +1,25 @@
 #!/usr/bin/R --verbose --no-save
 
+# run model against "validation" to generate "predictions" for submission
+
+restore <- function() {
+    # restore random forest model
+    if (!exists("model") & file.exists("data/model-rf.rds")) {
+        print("Restoring model ...")
+        model <- readRDS("data/model-rf.rds")
+    }
+
+    if (!exists("validation")) {
+        validation <- read.csv(
+            "data/pml-testing.csv", header = TRUE,
+            na.strings = c("NA", "#DIV/0!"), stringsAsFactors = FALSE
+        )
+    }
+}
+
+#
+# Function to write validation results to separate files
+#
 pml_write_files = function(x) {
     n = length(x)
     for (i in 1:n) {
@@ -14,9 +34,16 @@ pml_write_files = function(x) {
 # MAIN
 #
 
-if (file.exists("data/predictions-rf.rds")) {
-    print("Restoring predictions ...")
-    predictions <- readRDS("data/predictions-rf.rds")
-}
+# load model and validation data
+restore()
 
+# make predictions on validation data
+predictions <- predict(model, newdata = validation)
+
+# save predictions
+saveRDS(predictions, "data/predictions-rf.rds")
+
+# separate predictions: 1 per result
 pml_write_files(predictions)
+
+#EOF
